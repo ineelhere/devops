1f05c16e7efadf36b35eeeddb02164d4e242e5f2
ineelhere <you@example.com>
2022-06-26 18:10:20 +0530

    testing download in deployed app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+!apt apt install figlet -y
+from fileinput import filename
 import streamlit as st
 import os
 
@@ -5,4 +7,7 @@ st.title("Turn Text to Figlet")
 
 text = st.text_input("Enter Text:")
 
+if text:
+    os.system(f'figlet -c {text} > figlet.txt')
+    st.download_button("Download Figlet", filename="figlet.txt")
 
